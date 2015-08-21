1cbc2803cf5c133ea6e0aee811a1982573d99be7
timmyreilly <timreilly@live.com>
2015-08-21 13:33:48 -0700

    added build command

--- PythonApplication3/static_site_generator/prototypes.py
+++ PythonApplication3/static_site_generator/prototypes.py
@@ -21,6 +21,8 @@ settings.configure(
         ),
     STATIC_URL='/static/',
     SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+    SITE_OUTPUT_DIRECTORY=os.path.join(BASE_DIR, '_build'),
+    STATIC_ROOT=os.path.join(BASE_DIR, '_build', 'static'),
 )
 
 if __name__ == "__main__":
